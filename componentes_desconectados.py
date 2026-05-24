12d2e9ec41b2125a72e9e0a19b2ed488c3f36f95
guirafael <guilhermerafael347@gmail.com>
2026-05-24 20:55:32 -0300

    Adicionar Descrição do Script de Componentes Desconectados

--- componentes_desconectados.py
+++ componentes_desconectados.py
@@ -1,3 +1,8 @@
+# Este script tem o objetivo de identificar componentes desconectados em um modelo hidráulico isolado da Paulista.wntr
+# O script carrega o modelo, converte-o em um grafo, identifica componentes desconectados e verifica se os reservatórios estão na mesma componente.
+# Também visualiza as tubulações conectadas aos nós da componente desconectada e as tubulações que fazem a ligação entre a componente desconectada e a componente principal.
+# Isso é importante para entender o motivo da desconexão e planejar ações de correção no modelo hidráulico antes de aplicar o algoritmo de estudo.
+
 import wntr
 import networkx as nx
 
@@ -30,19 +35,19 @@ for i, comp in enumerate(componentes):
     print(f"\nReservatórios na componente {i+1}: {reservatorios_na_comp}")
 
 # VISUALIZAR A COMPONENTE DESCONECTADA
-# nos_comp2 = {'6000', '2759', '4366', '1393', '4365', 'R6719', '1392'}
+nos_comp2 = {'6000', '2759', '4366', '1393', '4365', 'R6719', '1392'}
 
-# print("Tubulações conectadas a esses nós:")
-# for pipe_name, pipe in wn.pipes():
-#   if pipe.start_node_name in nos_comp2 or pipe.end_node_name in nos_comp2:
-#     print(f"  {pipe_name}: {pipe.start_node_name} → {pipe.end_node_name}")
+print("Tubulações conectadas a esses nós:")
+for pipe_name, pipe in wn.pipes():
+  if pipe.start_node_name in nos_comp2 or pipe.end_node_name in nos_comp2:
+    print(f"  {pipe_name}: {pipe.start_node_name} → {pipe.end_node_name}")
 
-# print("\nTubulações que saem da componente 2 e chegam na componente 1:")
-# comp1 = set(componentes[0])
-# for pipe_name, pipe in wn.pipes():
-#   start_in_2 = pipe.start_node_name in nos_comp2
-#   end_in_2 = pipe.end_node_name in nos_comp2
-#   start_in_1 = pipe.start_node_name in comp1
-#   end_in_1 = pipe.end_node_name in comp1
-#   if (start_in_2 and end_in_1) or (start_in_1 and end_in_2):
-#     print(f"  {pipe_name}: {pipe.start_node_name} → {pipe.end_node_name}  ← TUBULAÇÃO DE LIGAÇÃO")
+print("\nTubulações que saem da componente 2 e chegam na componente 1:")
+comp1 = set(componentes[0])
+for pipe_name, pipe in wn.pipes():
+  start_in_2 = pipe.start_node_name in nos_comp2
+  end_in_2 = pipe.end_node_name in nos_comp2
+  start_in_1 = pipe.start_node_name in comp1
+  end_in_1 = pipe.end_node_name in comp1
+  if (start_in_2 and end_in_1) or (start_in_1 and end_in_2):
+    print(f"  {pipe_name}: {pipe.start_node_name} → {pipe.end_node_name}  ← TUBULAÇÃO DE LIGAÇÃO")
